38cf77807652221320ec8e678acb6d2aa5d75a4a
smercy <stevemercy4@gmail.com>
2018-01-13 14:46:35 -0600

    lesson 7 uploaded

---
+++ toptechprojects/lesson7/lesson7.ino
@@ -0,0 +1,93 @@
+/*
+Created on Thursday, Jan 13th 2018
+
+@author: smercy
+
+Replace for-loop with while-loop
+removed Prompt for on and delay time to simplify the program
+
+*/
+
+int redLED=9; // assing pin number 9 the a redLED variable
+int yellowLED=8; // assing pin number 8 the a yellowLED variable
+int redOnTime = 250; //the number in milliseconds the red LED stay on
+int redOffTime = 250; //the delay in milliseconds after red LED goes off
+int yellowOnTime = 250; //the number in milliseconds the red LED stay on
+int yelloOffTime = 250; //the delay in milliseconds after yellow LED goes off
+int numRedBlink; //number of times the red light blink.
+int numYellowBlink; //number of times the red light blink.
+String redMsg = "Red LED light blinking";
+String yellowMsg = "Yellow LED light blinking";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+
+  //allow user to interact with the program
+
+  //Red light block
+  Serial.println("How many times do you want the red LED to blink?"); //Prompt user for the number of time to blink the red light
+  while(Serial.available() == 0){ } // wait for user in input , 0 means there is no data and 1 means there is data.
+  numRedBlink = Serial.parseInt(); // read user inout and assign to numRedBlink variable
+
+  //Serial.println("How many miliseconds do you want the red LED to stay on?");
+  //while(Serial.available()== 0){ }
+  //redOnTime = Serial.parseInt();
+
+  //Serial.println("How many miliseconds do you want to delay after the red LED goes off ");
+  //while(Serial.available()== 0){ }
+  //redOffTime = Serial.parseInt();
+
+
+
+  //Yellow light block
+
+  Serial.println("How many times do you want the yellow LED to blink?"); //Prompt user for the number of time to blink the yellow light
+  while(Serial.available() == 0){ } // wait for user in input , 0 means there is no data and 1 means there is data.
+  numYellowBlink = Serial.parseInt(); // read user inout and assign to numYellowBlink variable
+
+  //Serial.println("How many miliseconds do you want the yellow LED to stay on?");
+  //while(Serial.available()== 0){ }
+  //yellowOnTime = Serial.parseInt();
+
+  //Serial.println("How many miliseconds do you want to delay after the yellow LED goes off");
+  //while(Serial.available()== 0){ }
+  //yelloOffTime = Serial.parseInt();
+
+}
+ String redLedStatus = "   blinking red LED #: ";
+ String yellowwLedStatus = "   blinking yellow LED #: ";
+void loop() {
+int j = 0;
+Serial.println(redMsg);
+  while (j <= 10) {
+    Serial.println( redLedStatus +String(j));
+    digitalWrite(redLED, HIGH);
+    delay(redOnTime);
+    digitalWrite(redLED, LOW);
+    delay(redOffTime);
+    j+=1;
+  }
+
+  Serial.println(" ");
+
+  int i = 0;
+  Serial.println(yellowMsg);
+  while (i <= 20) {
+  Serial.println(yellowwLedStatus + String(i));
+  digitalWrite(yellowLED, HIGH);
+  delay(yellowOnTime);
+  digitalWrite(yellowLED, LOW);
+  delay(yelloOffTime);
+  i+=1;
+
+  }
+
+Serial.println(" ");
+
+
+  // put your main code here, to run repeatedly:
+
+}
---
+++ toptechprojects/lesson7_while.ino
@@ -0,0 +1,22 @@
+/*
+
+Created on Thursday, Jan 13th 2018
+
+@author: smercy
+
+*/
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  int j = 1;
+   while (j<=10) {
+     print(j);
+     j+=1;
+   }
+
+
+}
